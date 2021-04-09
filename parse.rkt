5d6f0e3a9946ad8affbef55115941595c98ad447
baileywickham <baileywickham@gmail.com>
2021-04-08 16:29:34 -1000

    update

--- parse.rkt
+++ parse.rkt
@@ -11,25 +11,25 @@
      | (println LC)
 |#
 
-(provide parse ExprLC (struct-out LamC) (struct-out AppC) (struct-out PrimC))
+(provide parse ExprLC (struct-out LamLC) (struct-out AppLC) (struct-out PrimLC))
 
-(define-type ExprLC (U Real Symbol LamC AppC PrimC))
-(struct LamC ([arg : Symbol] [body : ExprLC]) #:transparent)
-(struct AppC ([f : ExprLC] [arg : ExprLC]) #:transparent)
-(struct PrimC ([name : Symbol] [args : (Listof ExprLC)]) #:transparent)
+(define-type ExprLC (U Real Symbol LamLC AppLC PrimLC))
+(struct LamLC ([arg : Symbol] [body : ExprLC]) #:transparent)
+(struct AppLC ([f : ExprLC] [arg : ExprLC]) #:transparent)
+(struct PrimLC ([name : Symbol] [args : (Listof ExprLC)]) #:transparent)
 
 ;; Given a function represented as an S-expression, translates it into an AST representation
 (define (parse [s : Sexp]) : ExprLC
   (match s
     [(or (? real?) (? symbol?)) s]
-    [`(/ ,(? symbol? id) => ,body) (LamC id (parse body))]
-    [`(,(or 'ifleq0 '+ '* 'println) ,args ...) (PrimC (car s) (map parse (cast args (Listof Sexp))))]
-    [`(,f ,a) (AppC (parse f) (parse a))]))
+    [`(lam ,(? symbol? id) ,body) (LamLC id (parse body))]
+    [`(,(or 'ifleq0 '+ '* 'println) ,args ...) (PrimLC (car s) (map parse (cast args (Listof Sexp))))]
+    [`(,f ,a) (AppLC (parse f) (parse a))]))
 
 ;; ---------------------------------------------------------------------------------------------------
 
 (module+ test
   (require typed/rackunit)
 
-  (check-equal? (parse '(+ 4 (* 5 6))) (PrimC '+ (list 4 (PrimC '* '(5 6)))))
-  (check-equal? (parse '((/ a => (+ a 1)) 3)) (AppC (LamC 'a (PrimC '+ '(a 1))) 3)))
+  (check-equal? (parse '(+ 4 (* 5 6))) (PrimLC '+ (list 4 (PrimLC '* '(5 6)))))
+  (check-equal? (parse '((lam a (+ a 1)) 3)) (AppLC (LamLC 'a (PrimLC '+ '(a 1))) 3)))
--- translation_c.rkt
+++ translation_c.rkt
@@ -1,55 +1,76 @@
-#lang typed/racket
+#lang typed/racket/no-check
 
 (require "parse.rkt")
 
-(define-type ExprC (U ExprLC FunC EnvC))
-(struct FunC ([name : Symbol]) #:transparent)
-(struct EnvC ([i : Integer]) #:transparent)
+(define-type ExprC (U FunC EnvC PrimC Real Symbol AppC LamC))
+(struct FunC ([name : Symbol] [len : Integer]) #:transparent)
+(struct EnvC ([i : Integer] [name : Symbol]) #:transparent)
+
+(struct LamC ([arg : Symbol] [body : ExprC] [len : Integer]) #:transparent)
+(struct AppC ([f : ExprC] [arg : ExprC]) #:transparent)
+(struct PrimC ([name : Symbol] [args : (Listof ExprC)]) #:transparent)
 
 (define-type ProgramC (Mutable-HashTable Symbol ExprC))
-
+(define-type Env (Listof Symbol))
 
 
 (define (top-translate [s : Sexp]) : Any
   (define funs : ProgramC (make-hash))
-  (define main (extract-funs (parse s) funs '()))
-  (string-join (hash-map funs c-translate-fun) "\n\n"))
+  (define main (extract-funs (uncover-env (parse s) '()) funs))
+  (string-join (append (hash-map funs c-translate-fun) (list (c-translate-main main))) "\n\n"))
 
 (define (extract-funs [e : ExprLC] [funs : ProgramC]) : ExprC
   (match e
+    [(? EnvC?) e]
     [(? real?) e]
     [(? symbol?) e]
-    [(PrimC name args) (PrimC name (map (λ ([arg : ExprLC]) (extract-funs arg funs env)) args))]
-    [(AppC f arg) (AppC (extract-funs f funs env) (extract-funs arg funs env))]
-    [(LamC arg body) (add-fun body funs env)]))
+    [(PrimC name args) (PrimC name (map (λ ([arg : ExprLC]) (extract-funs arg funs)) args))]
+    [(AppC f arg) (AppC (extract-funs f funs) (extract-funs arg funs))]
+    [(LamC arg body len) (add-fun body funs len)]))
 
-(define (uncover-env [e : ExprLC] [env: (Listof Symbol)]) : ExprLC
+(define (uncover-env [e : ExprLC] [env : Env]) : ExprC
   (match e
     [(? real?) e]
-    [(? symbol?) (index
+    [(? symbol?) (EnvC (index-of env e) e)]
+    [(PrimLC name args) (PrimC name (map (lambda ([arg : ExprLC]) (uncover-env arg env)) args))]
+    [(AppLC f arg) (AppC (uncover-env f env) (uncover-env arg env))]
+    [(LamLC arg body) (LamC arg (uncover-env body (cons arg env)) (length env))]))
 
-(define (add-fun [e : ExprC] [funs : ProgramC]) : FunC
+
+(define (add-fun [e : ExprC] [funs : ProgramC] [len : Integer]) : FunC
   (define name (gensym 'lambda))
-  (hash-set! funs name (extract-funs e funs env))
-  (FunC name))
+  (hash-set! funs name (extract-funs e funs))
+  (FunC name len))
 
 (define (c-translate [e : ExprC]) : String
   (match e
     [(? real?) (~v e)]
-    [(? symbol?) (~a e)]
+    [(EnvC i id) (format "env[~a /* ~a */]" i id)]
     [(PrimC 'println (list arg)) (format "printf(\"%d\\n\", (int) ~a)" (c-translate arg))]
     [(PrimC 'ifleq0 (list f t e))
      (format "((int) ~a <= 0 ? ~a : ~a)" (c-translate f) (c-translate t) (c-translate e))]
-    [(PrimC '+ (list a b)) (format "((int) ~a + (int) ~a)" a b)]
-    [(PrimC '* (list a b)) (format "((int) ~a * (int) ~a)" a b)]
-    [(AppC name arg) ] ))
+    [(PrimC '+ (list a b)) (format "((int) ~a + (int) ~a)" (c-translate a) (c-translate b))]
+    [(PrimC '* (list a b)) (format "((int) ~a * (int) ~a)" (c-translate a) (c-translate b))]
+    [(AppC f arg) (format "run_closure(~a, ~a)" (c-translate f) (c-translate arg))]
+    [(FunC name len) (format "create_closure(&~a, env, ~a)" name len)]))
 
 (define (c-translate-fun [s : Symbol] [e : ExprC]) : String
-  (format "void* ~a(void* env) {
+  (format "void* ~a(void** env) {
     return ~a;
 }" s (c-translate e)))
+
+(define (c-translate-main [e : ExprC]) : String
+  (format "int main() {
+    return ~a;
+}" (c-translate e)))
+
 
 (displayln (top-translate 5))
 (displayln (top-translate '(lam x (+ x 1))))
 (displayln (top-translate '(lam x (+ x ((lam y (ifleq0 y 1 -1)) 0)))))
-
+(displayln
+    (top-translate
+    '(println ((lam fact ((fact fact) 10))
+                (lam self
+                    (lam v
+                        (ifleq0 v 1 (* v ((self self) (+ v -1))))))))))
