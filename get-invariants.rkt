e40f6187ed704c1ea7d0d27cd165169629166dda
baileywickham <baileywickham@gmail.com>
2021-04-24 09:45:42 -1000

    update code

--- get-invariants.rkt
+++ get-invariants.rkt
@@ -5,7 +5,7 @@
 (require "parser.rkt")
 
 (struct Implies ([left : Any] [right : Any]) #:transparent)
-(struct Junction ([op : (U '^ 'v)] [elts : (Listof Any)]) #:transparent)
+(struct Junction ([op : (U '∧ 'v)] [elts : (Listof Any)]) #:transparent)
 
 (struct Substitution ([what : Any] [for : Symbol] [in : Any]) #:transparent)
 
@@ -16,7 +16,7 @@
 
 (define (omega [tau : Any] [I : Any] [cfg : CFG]) : Any
   (match tau
-    [(Assert p) (list (Junction '^ (list p I)))]
+    [(Assert p) (list (Junction '∧ (list p I)))]
     [(Assume p) (list (Implies p I))]
     ;;[(Assign var (void)) (Substitution (gensym 'r) var I)]
     [(Assign var val) (list (Substitution val var I))]
@@ -26,12 +26,27 @@
     [(Conditional pred t f) (append (omega (hash-ref cfg t) I cfg) (omega (hash-ref cfg f) I cfg))]
     ['() (list I)]))
 
+; (define (get-conditions [tau : Any] [cfg : CFG]) : Any
+
+(define (display-invariant [I : Any]) : Any
+  (match I
+    [(Implies left right) (format "~a => ~a" (display-invariant left) (display-invariant right))]
+    [(Junction op elts) (string-join (map display-invariant elts) (format " ~a " op))]
+    [(Substitution what for in) (format "~a[~a/~a]" (display-invariant in) (display-invariant what) (display-invariant for))]
+    [(Prim op a b) (format "~a ~a ~a" (display-invariant a) op (display-invariant b))]
+    [(? symbol? a) (~a a)]
+    [(? integer? a) (~a a)]))
+
+(define (display-invariants [I : Any]) : String
+  (string-join (map display-invariant I) "\n"))
+
+
 (omega (list (Assert 1) (Assign 'x 2)) 'I (make-hash))
-(get-invariants (make-CFG-program
+(displayln (display-invariants (get-invariants (make-CFG-program
     (list
     (Assign 'x -50)
     (While (Prim '< 'x 0)
            (list
             (Assign 'x (Prim '+ 'x 'y))
             (Assign 'y (Prim '+ 'y 1))))
-    (Assert (Prim '> 'y 0)))))
+    (Assert (Prim '> 'y 0)))))))
