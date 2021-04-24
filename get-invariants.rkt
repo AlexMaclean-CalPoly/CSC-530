424dccf65c7fa60cd77da9217a657cf1c7c86412
baileywickham <baileywickham@gmail.com>
2021-04-24 11:08:05 -1000

    update code

--- get-invariants.rkt
+++ get-invariants.rkt
@@ -14,9 +14,12 @@
 
 (define (get-invariants [cfg : CFG]) : Any
   (append-map (lambda ([cp : Cut-Point])
-                (Implies
-                  (Junction '∧ (get-conditions (hash-ref cfg cp) cfg (make-immutable-hash)))
+                (map Implies
+                  (map (lambda ([x : Any]) (Junction '∧ (cons (if (start-point? cp) true 'I) x))) (get-conditions (hash-ref cfg cp) cfg (make-immutable-hash)))
                   (omega (hash-ref cfg cp) 'I cfg))) (filter Cut-Point? (hash-keys cfg))))
+
+(define (start-point? [c : Cut-Point]) : Boolean
+  (equal? (Cut-Point-name c) 'start))
 
 (define (omega [tau : Any] [I : Any] [cfg : CFG]) : Any
   (match tau
@@ -27,7 +30,7 @@
     [(cons S1 S2) (append-map (lambda ([t : Any]) (omega S1 t cfg)) (omega S2 I cfg))]
     [(GoTo (Cut-Point _)) (list I)]
     [(GoTo label) (omega (hash-ref cfg label) I cfg)]
-    [(Conditional pred t f) (append (omega (hash-ref cfg t) I cfg) (omega (hash-ref cfg f) I cfg))]
+    [(Conditional pred t f) (append (omega (GoTo t) I cfg) (omega (GoTo f) I cfg))]
     ['() (list I)]))
 
 (define (subst-exp [e : Exp] [env : (HashTable Symbol Exp)]) : Exp
@@ -36,16 +39,17 @@
     [(? integer?) e]
     [(Prim op a b) (Prim op (subst-exp a env) (subst-exp b env))]))
 
-(define (get-conditions [tau : Any] [cfg : CFG] [env : (HashTable Symbol Exp)]) : Any
+(define (get-conditions [tau : Any] [cfg : CFG] [env : (HashTable Symbol Exp)]) : Any 
   (match tau
-    [(cons (Assign var val) rst) (get-conditions tau cfg (hash-set env var (subst-exp val env)))]
+    [(cons (Assign var val) rst) (get-conditions rst cfg (hash-set env var (subst-exp val env)))]
     [(cons (GoTo (? Cut-Point?)) '()) '(())]
     [(cons (GoTo label) '()) (get-conditions (hash-ref cfg label) cfg env)]
     [(Conditional pred t f)
      (append (map (lambda ([c : Any]) (cons pred c))
-                  (get-conditions (hash-ref cfg t) cfg env))
+                  (get-conditions (list (GoTo t)) cfg env))
              (map (lambda ([c : Any]) (cons (Not pred) c))
-                  (get-conditions (hash-ref cfg f) cfg env)))]))
+                  (get-conditions (list (GoTo f)) cfg env)))]
+    [(cons _ rst) (get-conditions rst cfg env)]))
 
 (define (display-invariant [I : Any]) : Any
   (match I
@@ -56,7 +60,8 @@
     [(Prim op a b) (format "~a ~a ~a" (display-invariant a) op (display-invariant b))]
     [(? symbol? a) (~a a)]
     [(? integer? a) (~a a)]
-    [(Not elt) (format "¬~a" (display-invariant elt))]))
+    [(? boolean? b) (if b "true" "false")]
+    [(Not elt) (format "¬(~a)" (display-invariant elt))]))
 
 (define (display-invariants [I : Any]) : String
   (string-join (map display-invariant I) "\n"))
