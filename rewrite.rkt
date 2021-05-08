4f959d5f4d5f81201b0440a3b2cb40b221c20a06
baileywickham <baileywickham@gmail.com>
2021-05-08 09:27:48 -1000

    update code

--- rewrite.rkt
+++ rewrite.rkt
@@ -1,8 +1,9 @@
-#lang typed/racket
+#lang typed/racket/no-check
 
 (require "types.rkt")
+(require racket/hash) 
 
-(define EM : Error-Model '(#s(Rewrite-Rule #s(Arith a + b) #s(Arith a #s(Choices (- * /)) b))))
+(define EM : Error-Model '(#s(Rewrite-Rule #s(BinOp #s(Subst a) + #s(Subst b)) #s(BinOp #s(Subst a) #s(Choices (- * /)) #s(Subst b)))))
 
 ;;
 (define (rewrite [term : ArithExpr] [em : Error-Model]) : ArithExpr~
@@ -10,15 +11,29 @@
    (cons (match term
            [(BinOp a op b) (BinOp (rewrite a em) op (rewrite b em))]
            [(or (? integer?) (? symbol?)) term])
-         (rewrite* ))))
+         (rewrite* term em))))
 
 ;;
-(define (rewrite* [term : ArithExpr] [em : Error-Model]) : (Listof Arith-Set-Expr)
+(define (rewrite* [term : ArithExpr] [em : Error-Model]) : (Listof ArithExpr~)
   (match em
     ['() '()]
     [(cons (Rewrite-Rule before after) rst)
-     (let [(r (rewrite* term rst)) (b (before-matches? before term))]
-       (if b (cons (apply-after term b) r) r))]))
+     (let [(r (rewrite* term rst)) (b (before-matches? before term))] 
+       (if b (cons (apply-after b after) r) r))]))
 
+(define (before-matches? [before : (ArithExpr* Subst)] [term : ArithExpr]) : (U False (Immutable-HashTable Subst ArithExpr))
+  (match* (before term)
+    [((? Subst?) _) (make-immutable-hash (list (cons before term)))]
+    [((BinOp a0 op0 b0) (BinOp a1 op1 b1)) (let [(a (before-matches? a0 a1)) (b (before-matches? b0 b1)) (op (before-matches? op0 op1))]
+                                             (and a b op (hash-union a b op)))]
+    [(_ _) (and (equal? before term) (make-immutable-hash '()))]))
 
+(define (apply-after [env : (Immutable-HashTable Subst ArithExpr)] [after : (ArithExpr~* Subst)]) : ArithExpr~
+  (match after
+    [(? Subst?) (hash-ref env after)]
+    [(BinOp a op b) (BinOp (apply-after env a) (cast (apply-after env op) ArithOp~) (apply-after env b))]
+    [(Choices clauses) (Choices (map (lambda ([a : (ArithExpr~* Subst)]) (apply-after env a)) clauses))]
+    [(or (? integer?) (? symbol?)) after]))
+                                           
+(rewrite (BinOp 3 '+ 4) EM)
 
--- types.rkt
+++ types.rkt
@@ -3,18 +3,21 @@
 (provide (all-defined-out))
 
 ;; normal mPy
-(define-type ArithExpr (U Integer Symbol (BinOp ArithExpr ArithOp)))
+(define-type ArithExpr (ArithExpr* Nothing))
+(define-type (ArithExpr* P) (U P Integer Symbol (BinOp ArithExpr ArithOp)))
 (define-type ArithOp (U '+ '* '- '/ '**))
 
 ;; rewritten ~mPy~
-(define-type ArithExpr~ (U Integer Symbol (BinOp ArithExpr~ ArithOp~) (Choices ArithExpr~)))
+(define-type ArithExpr~ (ArithExpr~* Nothing))
+(define-type (ArithExpr~* P) (U Integer Symbol (BinOp ArithExpr~ ArithOp~) (Choices ArithExpr~) P))
 (define-type ArithOp~ (U ArithOp (Choices ArithOp~)))
 
-(struct (A) Choices ([s : (Listof A)]) #:prefab)
+(struct (A) Choices ([clauses : (Listof A)]) #:prefab)
 
 ;; shared
 (struct (A O) BinOp ([a0 : A] [op : O] [a1 : A]) #:prefab)
 
 ;; Error Model
+(struct Subst ([s : Symbol]) #:prefab)
 (define-type Error-Model (Listof Rewrite-Rule))
-(struct Rewrite-Rule ([before : ArithExpr] [after : ArithExpr~]) #:prefab)
+(struct Rewrite-Rule ([before :  (ArithExpr* Subst)] [after : (ArithExpr~* Subst)]) #:prefab)
