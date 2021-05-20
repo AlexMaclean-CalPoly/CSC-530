57c0cb044608b68db5e5a91ac2ca1187928c9ddb
Alex MacLean <alex.maclean2000@gmail.com>
2021-05-20 09:42:14 -0700

    cleanup

--- hw1/classify.rkt
+++ hw1/classify.rkt
@@ -2,26 +2,24 @@
 
 (provide (all-defined-out))
 
-; Takes as input a propositional formula and returns
-; * 'TAUTOLOGY if every interpretation I satisfies F;
-; * 'CONTRADICTION if no interpretation I satisfies F;
-; * 'CONTINGENCY if there are two interpretations I and I′ such that I satisfies F and I' does not.
+;; Takes as input a propositional formula and returns
+;; * 'TAUTOLOGY if every interpretation I satisfies F;
+;; * 'CONTRADICTION if no interpretation I satisfies F;
+;; * 'CONTINGENCY if there are two interpretations I and I′ such that I satisfies F and I' does not.
 (define (classify F)
-  (define I1 (solve (assert F)))
-  (define I2 (solve (assert (! F))))
-  (cond [(unsat? I2) 'TAUTOLOGY]
-        [(unsat? I1) 'CONTRADICTION]
+  (cond [(unsat? (solve (assert (! F)))) 'TAUTOLOGY]
+        [(unsat? (solve (assert F))) 'CONTRADICTION]
         [else 'CONTINGENCY]))
 
 (define-symbolic* p q r boolean?)
 
-; (p → (q → r)) → (¬r → (¬q → ¬p))
+;; (p → (q → r)) → (¬r → (¬q → ¬p))
 (define f0 (=> (=> p (=> q r)) (=> (! r) (=> (! q) (! p)))))
 
-; (p ∧ q) → (p → q)
+;; (p ∧ q) → (p → q)
 (define f1 (=> (&& p q) (=> p q)))
 
-; (p ↔ q) ∧ (q → r) ∧ ¬(¬r → ¬p)
+;; (p ↔ q) ∧ (q → r) ∧ ¬(¬r → ¬p)
 (define f2 (&& (<=> p q) (=> q r) (! (=> (! r) (! q)))))
 
 (module+ test
