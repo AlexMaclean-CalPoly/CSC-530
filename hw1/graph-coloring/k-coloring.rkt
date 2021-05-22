f248f2ed13e40d1d0e8c0453f6fb438672595f6a
Alex MacLean <alex.maclean2000@gmail.com>
2021-05-22 11:44:26 -0700

    cleanup

--- hw1/graph-coloring/k-coloring.rkt
+++ hw1/graph-coloring/k-coloring.rkt
@@ -5,7 +5,7 @@
 (provide
  k-coloring      ; (->* (graph/c natural-number/c) [(is-a? solver%)] (or/c #f coloring/c))
  valid-coloring? ; (-> graph/c coloring/c boolean?)
-)
+ )
 
 ; Returns true iff the given coloring is correct for the specified graph.
 (define (valid-coloring? graph coloring)
@@ -18,9 +18,13 @@
 ; be colored with k colors.  Otherwise returns #f.
 (define (k-coloring graph k)
   (define v (vector-length graph))
-  (define vertex-constraint (build-list v (lambda (index) (build-list k (lambda (i) (+ i (* k index) 1))))))
-  (define edge-constraint (append-map (lambda (w edges)
-                                        (append-map (lambda (v) (build-list k (lambda (c) (list (- (+ c (* k v) 1)) (- (+ c (* k w) 1)))))) edges))
-                                                    (range v) (vector->list graph)))
+  (define vertex-constraint (build-list v (λ (index) (build-list k (λ (c) (+ c (* k index) 1))))))
+  (define edge-constraint (append-map (λ (w edges)
+                                        (append-map (λ (v)
+                                                      (build-list k (λ (c)
+                                                                      (list (- (+ c (* k v) 1))
+                                                                            (- (+ c (* k w) 1))))))
+                                                    edges))
+                                      (range v) (vector->list graph)))
   (define solution (solve (append vertex-constraint edge-constraint)))
   (displayln solution))
